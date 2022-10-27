4a82a5abfae87679e8b0932bc87fd694dede6cde
sifatullahsu <personal.sifat@gmail.com>
2022-10-27 12:45:43 +0600

    README.md file

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ScrollToTop from '../components/ScrollToTop';
 
 const Main = () => {
   return (
     <div>
+      <ScrollToTop></ScrollToTop>
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
