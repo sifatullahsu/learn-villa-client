4b960fd33a5221f2df7d3d0912db3bfec36394b8
sifatullahsu <personal.sifat@gmail.com>
2022-12-20 01:20:30 +0600

    hero section updated

--- src/components/Course.js
+++ src/components/Course.js
@@ -7,7 +7,7 @@ const Course = ({ course }) => {
       <div className="card rounded-0">
         <img src={course.picture} className="card-img-top img-fluid rounded-0 p-3 pb-0" alt="..." />
         <div className="card-body">
-          <h5 className="card-title fs-6 fw-bold">{course.name}</h5>
+          <h5 className="card-title fs-6 fw-bold text-truncate">{course.name}</h5>
           <p className="card-text text-muted crop-text-2">{course.short_description}</p>
           <div className='d-flex justify-content-between align-items-end pt-3'>
             <p className='fw-bold mb-0'>Price: ${course.price}</p>
